6ab31523c02cce31cac6b3c87e4c00a37b5fa4d0
kartikpathak414 <kartikpathak414@gmail.com>
2026-03-26 12:49:13 +0530

    UC2: Display Hello with user name

--- src/HelloApp.java
+++ src/HelloApp.java
@@ -1,4 +1,11 @@
 public class HelloApp {
     public static void main(String[] args) {
-        System.out.println("Hello World UC1");    }
+
+        if (args.length > 0) {
+            System.out.println("Hello " + args[0]);
+        } else {
+            System.out.println("Hello World");
+        }
+
+    }
 }
